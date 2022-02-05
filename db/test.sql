3bd82e12ec7c37a1e6f7cf5fa0dafc73ea43b75f
Andy Humes <andrew.humes@du.edu>
2022-02-04 19:26:37 -0700

    changed names

---
+++ db/test.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+CREATE TABLE employeeDemos (
+	id INT NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(30),
+    lastName VARCHAR(30)
+    );
+
+
+USE employees_db;
